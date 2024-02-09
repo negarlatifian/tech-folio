48e80204850dbf74e34e3261a8e4aae636c6a696
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2024-02-09 21:23:15 +0100

    made titles responsive

--- components/ui/Titles.tsx
+++ components/ui/Titles.tsx
@@ -4,17 +4,19 @@ const Titles = () => {
   return (
     <ul className='flex flex-col w-full gap-6 mt-8'>
       <li className='flex flex-col'>
-        <p className='font-sans text-5xl uppercase text-right'>creative</p>
+        <p className='font-sans md:text-5xl text-[2rem] uppercase text-right'>
+          creative
+        </p>
         {/* <span className='w-44 border-t-2 float-end'></span> */}
       </li>
       <li className='flex flex-col overline'>
-        <p className='font-sans text-5xl uppercase text-right underline underline-offset-8 tracking-tight '>
+        <p className='font-sans md:text-5xl text-[2rem] uppercase text-right underline underline-offset-8 tracking-tight '>
           web developer
         </p>
         {/* <span className='w-44 border-t-2'></span> */}
       </li>
       <li className='flex flex-col'>
-        <p className='font-sans text-5xl uppercase text-right underline underline-offset-8 tracking-wider '>
+        <p className='font-sans md:text-5xl text-[2rem] uppercase text-right underline underline-offset-8 tracking-wider '>
           web designer
         </p>
         {/* <span className='w-44 border-t-2'></span> */}
