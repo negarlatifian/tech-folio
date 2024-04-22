8ac1d777be2cd2a795d1dfa39ca271bf81c00c88
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2024-04-22 10:16:44 +0200

    commented out scene

--- components/ui/Scene.tsx
+++ components/ui/Scene.tsx
@@ -1,18 +1,18 @@
-'use client';
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
-import Model from './Model';
-import { Environment } from '@react-three/drei';
+// 'use client';
+// import React from 'react';
+// import { Canvas } from '@react-three/fiber';
+// import Model from './Model';
+// import { Environment } from '@react-three/drei';
 
-const Scene = () => {
-  return (
-    <Canvas>
-      <ambientLight color={'#39566a'} intensity={7} />
-      {/* <directionalLight intensity={3} position={[0, 3, 2]} /> */}
-      <Environment preset='city' map={null} />
-      <Model />
-    </Canvas>
-  );
-};
+// const Scene = () => {
+//   return (
+//     <Canvas>
+//       <ambientLight color={'#39566a'} intensity={7} />
+//       {/* <directionalLight intensity={3} position={[0, 3, 2]} /> */}
+//       <Environment preset='city' map={null} />
+//       <Model />
+//     </Canvas>
+//   );
+// };
 
-export default Scene;
+// export default Scene;
